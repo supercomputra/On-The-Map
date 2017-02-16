aab8f9b481d2adbee03058f3fb9a1cdf49405734
Zulwiyoza Putra <zulwiyozaputra@gmail.com>
2017-02-16 15:11:23 +0700

    Parse convenience added

---
+++ On The Map/ParseConvenience.swift
@@ -0,0 +1,13 @@
+//
+//  ParseConvenience.swift
+//  On The Map
+//
+//  Created by Zulwiyoza Putra on 2/16/17.
+//  Copyright © 2017 zulwiyozaputra. All rights reserved.
+//
+
+import Foundation
+
+extension ParseClient {
+    
+}
